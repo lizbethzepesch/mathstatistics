a280039e1b29c9bce89628c74ed055d9fd9732d1
Yelyzaveta Kliuieva <elizbetti15@gmail.com>
2021-05-21 21:12:49 +0300

    task5 sprint1 completed

--- sprint01/js/main.js
+++ sprint01/js/main.js
@@ -231,25 +231,47 @@ function task3(sample, name) {
 
 }
 
-function calcsForTask4(sample_){
+function calcsForTask4(sample){
     let res = []
     let temp = 0
     let temp1 = 0
     let temp2 = 0
-    let data = []
-    data.push([])
-    data.push([])
+    let arr = []
+    arr.push([])
+    arr.push([])
 
-    sampleFilter(sample_, data)
+    sampleFilter(sample, arr)
 
+    let array = []
+    for (let i = 0; i < samples.length; i++) {
+        if (samples[i].value == "") continue 
+        array = sum(array, samples[i].value.split(' '))
+    }
+    for (let i = 0; i < array.length; i++) {
+        array[i] = array[i].replace(',', '.')
+        array[i] = parseFloat(array[i])
+    }
+    array.sort((a, b) => a - b)
+
+    for (let i = 0; i < arr[0].length; i++) {
+        temp += arr[0][i] * arr[1][i]
+    }
+    temp /= array.length
+
+    let d1 = 0
+    for (let i = 0; i < arr[0].length; i++) {
+        d1 += arr[0][i] * arr[0][i] * arr[1][i]
+    }
+    d1 /= array.length
+    d1 -= temp ** 2
     
 
     res.push(temp.toFixed(5))
-    res.push(temp1.toFixed(5))
-    res.push(Math.sqrt(temp1).toFixed(5))
-    res.push((temp + temp * 0.0001).toFixed(5))
-    res.push((temp1 + temp1 * 0.0001).toFixed(5))
-    res.push(Math.sqrt((temp1 + temp1 * 0.0001)).toFixed(5))
+    res.push(temp.toFixed(5))
+    res.push(temp.toFixed(5))
+    res.push(temp.toFixed(5))
+    res.push(temp.toFixed(5))
+    res.push(temp.toFixed(5))
 
 
     return res
@@ -275,17 +297,64 @@ function task4(sample, name) {
 
 }
 
+function zScore(p){
+    if (p > 1) 
+        p *= 0.01
+    
+    if (p < 0.5) 
+        return -1 * zScore(1 - p)
+    
+    if (p > 0.92) {
+        if (p === 1) {
+            return Infinity
+        }
+
+        const temp = Math.sqrt(-1 * Math.log(1 - p))
+        
+        return (((2.3212128 * temp + 4.8501413) * temp - 2.2979648) * temp - 2.7871893) / ((1.6370678 * temp + 3.5438892) * temp + 1)
+    }
+
+    p -= 0.5
+
+    const temp = Math.pow(p, 2)
+
+    return p * (((-25.4410605 * temp + 41.3911977) * temp - 18.6150006) * temp + 2.5066282) / ((((3.1308291 * temp - 21.0622410) * temp + 23.0833674) * temp - 8.4735109) * temp + 1)
+}
+
 function calcsForTask5(sample){
     let res = []
     let temp = 0
+    let zS = zScore(0.95).toFixed(5)
 
-    res.push(temp.toFixed(5))
-    res.push(temp.toFixed(5))
-    res.push(temp.toFixed(5))
-    res.push(temp.toFixed(5))
-    res.push(temp.toFixed(5))
-    res.push(temp.toFixed(5))
+    let arr = []
+    arr.push([])
+    arr.push([])
+    sampleFilter(sample, arr)
 
+    res.push(math.mean(sample).toFixed(5))
+
+    let temp1 = 0
+    for (let i = 0; i < arr[0].length; i++) {
+        temp1 += arr[0][i] * arr[1][i]
+        temp += arr[1][i]
+    }
+    temp1 /= temp
+
+    let temp2 = 0
+    for (let i = 0; i < arr[0].length; i++) {
+        temp2 += ((arr[0][i] - temp1) ** 2) * arr[1][i]
+    }
+    temp2 /= temp
+
+    let s2 = temp / (temp - 1) * temp2
+    let temp3 = Math.sqrt(s2)
+
+    
+    res.push((temp1 - (zS * temp3 / Math.sqrt(temp))).toFixed(5))
+    res.push((temp1 + (zS * temp3 / Math.sqrt(temp))).toFixed(5))
+    res.push(Math.sqrt(temp2).toFixed(5))
+    res.push(Math.sqrt(temp2 - (zS * temp3 / Math.sqrt(temp))).toFixed(5))
+    res.push(Math.sqrt(temp2 + (zS * temp3 / Math.sqrt(temp))).toFixed(5))
 
     return res 
 }
@@ -312,7 +381,8 @@ function task5(sample, name) {
 
 document.querySelector("input.calculate").addEventListener("click", function () {
     // let functions = [task1, task2, task3, task4, task5]
-    let functions = [task4, task5]
+    let functions = [task5]
+
     for (let i = 0; i < functions.length; i++) {
 
         if (i === 1) {
