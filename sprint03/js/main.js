2e4b901494f2b1ebfc5e8004556dcda9d6d8cd93
Yelyzaveta Kliuieva <elizbetti15@gmail.com>
2021-05-24 20:41:40 +0300

    fixed fucking js problem

---
+++ sprint03/js/main.js
@@ -0,0 +1,24 @@
+var fA = 0
+var fB = 0
+
+function myddA(){
+    fA++
+}
+
+function mydelA(){
+    fA--
+}
+
+function myaddB(){
+    fB++
+
+    for(let i = 0; i < fB; i++){
+        document.querySelector('#b1').appendChild(document.createElement("td").className = )
+        console.log(document.querySelector('#b1').innerHTML)
+    }
+    
+}
+
+function mydelB(){
+    fB--
+}
