24dc4105d7f2724dada2dd1eb9162f2c18782506
Yelyzaveta Kliuieva <elizbetti15@gmail.com>
2021-05-25 19:21:04 +0300

    input completed (sprin03)

--- sprint03/js/main.js
+++ sprint03/js/main.js
@@ -2,23 +2,52 @@ var fA = 0
 var fB = 0
 
 function myddA(){
-    fA++
+    let b = fB + 1
+    let a = fA + 3
+
+    fB++
+    let res = ''
+    res += `<tr id='b`+ (b + 2)+`'>  <td id="fb` + (b) + `">B` + (b + 1) + `</td>`
+    
+    for(let i = 0; i < fA + 3; i++)
+        res += `<td id='` + 'tdB' + (b + 2) +'A' + i + `'><input type='text' name='sample' value=""> </td>`
+    res += `</tr>`
+   
+    
+    document.querySelector('#table').innerHTML += res   
 }
 
 function mydelA(){
-    fA--
+    if(fB === 0) return
+    let b = fB + 2
+    document.querySelector('#b' + (b)).remove()
+    fB--
 }
 
 function myaddB(){
-    fB++
+    let a = fA + 3
+    fA++
 
-    for(let i = 0; i < fB; i++){
-        document.querySelector('#b1').appendChild(document.createElement("td").className = )
-        console.log(document.querySelector('#b1').innerHTML)
+    document.querySelector('#factorsA').innerHTML += '<td id="fa' + a + '">A' + (a) + '</td>'
+
+    for(let i = 0; i < fB + 2; i++){
+        let td = document.createElement("td")
+        td.id = 'tdB' + (i + 1) +'A' + a
+        document.querySelector('#b' + (i + 1)).appendChild(td)
+        document.querySelector('#tdB' + (i + 1) +'A' + a).appendChild(document.createElement("input"))
     }
+      
     
 }
 
 function mydelB(){
-    fB--
+    if(fA === 0) return
+    
+    fA--
+    let a = fA + 3
+    document.querySelector('#fa' + (a )).remove()
+    
+    for(let i = 0; i < fB + 2; i++)
+        document.querySelector('#tdB' + (i + 1) +'A' + a).remove()
+    
 }
