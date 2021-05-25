05d8205c51731046cf263d99d52916e2d02b3264
Yelyzaveta Kliuieva <elizbetti15@gmail.com>
2021-05-25 22:17:03 +0300

    v0.2

--- sprint03/js/main.js
+++ sprint03/js/main.js
@@ -1,6 +1,8 @@
 var fA = 0
 var fB = 0
 
+var samples = []
+
 function myddA(){
     let b = fB + 1
     let a = fA + 3
@@ -25,7 +27,7 @@ function mydelA(){
 }
 
 function myaddB(){
-    let a = fA + 3
+    let a = fA + 4
     fA++
 
     document.querySelector('#factorsA').innerHTML += '<td id="fa' + a + '">A' + (a) + '</td>'
@@ -50,4 +52,83 @@ function mydelB(){
     for(let i = 0; i < fB + 2; i++)
         document.querySelector('#tdB' + (i + 1) +'A' + a).remove()
     
+}
+
+
+
+function task1(){
+    parseInput()
+    let res =  `<table> <tr><td rowspan="3" colspan="2">Factor B</td> <td colspan="` + (3 + fA + 3 + fA) + `">Factor A</td> `
+    let table = document.querySelector('#input') 
+
+
+    
+    res += '<td rowspan="3">Rows mean</td>'
+    res += '<td rowspan="3">General mean</td>'
+    res += '</tr>'
+
+    res += `<tr id='factorsA'><td colspan="2">A1</td>
+    <td colspan="2">A2</td>
+    <td colspan="2">A3</td>
+`
+
+    for(let i = 0; i < fA; i ++)
+        res += ` <td colspan="2">A` + (i+4) + `</td>`
+ 
+
+    res += '</tr>'
+
+    res += '<tr>'
+    for(let i = 0; i < fA + 3; i ++){
+        res += ` <td>Block ` + (i+1) + `</td>`
+        res += ` <td>Block<br> mean</td>`
+    }
+    res += '</tr>'
+
+  
+    let j = 0
+    for(let i = 0; i < fB + 2; i ++){
+        res += '<tr>'
+        res += '<td>B' + (i+1) + '</td>'
+        res += '<td>Block ' + (i+1)+ '</td>'
+        
+            for(; j < fA + 3 * (i + 1); j++){
+                res += '<td>' + samples[j ].join(' ')+ '</td>'
+                res += '<td>'  + math.mean(samples[j]).toFixed(1)+ '</td>'
+            }
+            
+        res += '</tr>' 
+    }
+    j = 0
+   
+
+   
+    res += '<tr>'
+    res += '<td colspan="2">Columns<br>mean</td>'
+    for(let i = 0; i < fA + 3; i ++){
+        res += '<td>colspan="2"' + math.mean(samples[i], samples[i + 3]) + '</td>'
+
+       
+    }
+
+    res += '</tr>'
+
+
+    res += '</table>'
+
+
+    document.querySelector('#task1').innerHTML = res
+
+}
+
+function parseInput(){
+    for(let i = 0; document.getElementsByTagName('input')[i]; i ++)
+        if(document.getElementsByTagName('input')[i].value)
+            samples.push(document.getElementsByTagName('input')[i].value.split(' ').map(value => +value).filter(value => !isNaN(value)))
+    
+    samples = samples.filter(arr => arr.length)
+}
+
+function calc(){
+    task1()
 }
